9c8cee7420e7a40de091e6d2b8b249230a8bb263
Capa2 <johan931@gmail.com>
2021-03-06 10:00:16 +0100

    add some comments

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,14 +1,19 @@
 package server;
 
 import server.connection.Listener;
+import server.connection.ResponseProtocol;
+
 import java.net.Socket;
 import java.util.Vector;
 
 public class Server {
     Vector<Socket> connections;
     Listener incomming;
+    ResponseProtocol responseProtocol;
     public Server(int port) {
-        connections = new Vector<Socket>();
-        incomming = new Listener(connections, port);
+        connections = new Vector<Socket>(); // thread safe storage of client sockets
+        incomming = new Listener(connections, port); // scans for new connections and put them in connections vector
+        responseProtocol = new ResponseProtocol(connections); // send data back to connections
     }
+    // decision making processes should possibly happen here, or in a new class. the three classes above should have a singular purpose.
 }
--- src/main/java/server/connection/InputScanner.java
+++ src/main/java/server/connection/InputScanner.java
@@ -7,28 +7,30 @@ import java.util.Vector;
 
 public class InputScanner implements Runnable, Closeable {
     Vector<Socket> connections;
+    boolean open;
 
     public InputScanner(Vector<Socket> connections) {
         this.connections = connections;
+        open = false;
     }
 
     @Override
     public void run() {
-        //while (????) {
+        while (open) {
             scan(connections);
-        //}
+        }
     }
 
     @Override
-    public void close() throws IOException {
-
+    public void close() {
+        open = false;
     }
 
     private void scan(Vector<Socket> sockets) {
         sockets.forEach((socket) -> {
                     try {
                         if (socket.getInputStream().available() > 0) {
-                        // what to do if a client has sent data to the server
+                        // what to do if a client has sent data to the server...  send to Server class? responseprotocol?
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
