57ea7ecf546dc8a5f5f8e3755af9498d46d77782
Johan Karpantschof <johan931@gmail.com>
2021-03-10 13:31:39 +0100

    commented out bugged SEND code

--- src/main/java/server/connection/IoProtocol.java
+++ src/main/java/server/connection/IoProtocol.java
@@ -57,18 +57,18 @@ public class IoProtocol implements Runnable, Closeable {
         }
         // SEND PROTOCOL
         else if (token.equals("SEND")) {
-            String[] rx = new String[0]; // String array for storing handles for multiple recipients
+            //String[] rx = new String[0]; // String array for storing handles for multiple recipients
             if (tokenCount == 3) {
                 String receiver = tokenizer.nextToken();
                 String message = tokenizer.nextToken();
-                if (receiver.contains(",")) {
+                /* if (receiver.contains(",")) {
                     rx = receiver.split(","); // separates usernames in case of multiple recipients
-                }
+                }*/
                 for (Session s : sessions) {
-                    if(rx.length != 0) for (int i = 0; i <= rx.length - 1; i++) {  // sends message to each recipient in case of multiple recipients
+                 /* for (int i = 0; i <= rx.length - 1; i++) {  // sends message to each recipient in case of multiple recipients
                         if (s.getUser() == null || !rx[i].equals(s.getUser()) && !rx[i].equals("*")) continue;
                         s.push("MESSAGE#" + message);
-                    }
+                    }*/
                     if (s.getUser() == null || !receiver.equals(s.getUser()) && !receiver.equals("*")) continue;
                     s.push("MESSAGE#" + message);
                 }
